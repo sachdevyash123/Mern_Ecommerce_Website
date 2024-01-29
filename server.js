6a3f8aa88f0ddd9b04767274db0e8d2357ca1859
sachdevyash123 <140938066+sachdevyash123@users.noreply.github.com>
2024-01-29 18:13:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,16 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import cors from 'cors'
 import productRoutes from './routes/productRoutes.js'
 import path from "path"
+import {fileURLToPath} from 'url';
 //configure env
 dotenv.config()
 
 //connectDB
 connectDB();
 
+//esmodule fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 //rest object
 const app=express()
 
